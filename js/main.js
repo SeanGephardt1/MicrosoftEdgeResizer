4d0ffef3e9791becc94cc7bfdc938bde336a4db1
Sean Gephardt <seangephardt@live.com>
2020-10-16 11:42:04 -0700

    Resizer WIP

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-﻿
+﻿( function ()
+{
+	console.log( "BEGIN", new Date().toISOString() );
+	try
+	{
+		window.document.addEventListener( "DOMContentLoaded", function ( e ) 
+		{
+			console.debug( "DOMContentLoaded" );
+
+
+			return;
+		} );
+	}
+	catch ( ex )
+	{
+		console.log( "CATCH", new Date().toISOString() );
+		console.error( ex.number, ":", ex.name, ":", ex.message );
+		console.error( "exception stack::", ex.stack );
+		return;
+	}
+	finally
+	{
+		console.log( "FINALLY", new Date().toISOString() );
+	}
+	console.log( "END", new Date().toISOString() );
+	return;
+} )();
---
+++ js/popup.js
@@ -0,0 +1,34 @@
+﻿const sendMessageId = document.getElementById( "customize-btn" );
+
+if ( sendMessageId ) 
+{
+    sendMessageId.onclick = function ()
+    {
+        // do something
+        console.debug( "do something" );
+        chrome.tabs.query( { active: true, currentWindow: true },
+            function ( tabs )
+            {
+                chrome.tabs.sendMessage(
+                    tabs[0].id,
+                    {
+                        url: chrome.extension.getURL("img/resizer-original.jpg"),
+                        imageDivId: `${guidGenerator()}`,
+                        tabId: tabs[0].id
+                    },
+                    function ( response )
+                    {
+                            window.close();
+                    });
+                function guidGenerator() 
+                {
+                    const S4 = function () 
+                    {
+                        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                    };
+                    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+                }
+        });
+        return;
+    };
+}
